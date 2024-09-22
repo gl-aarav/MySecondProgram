3f25c7b2829e81c82effea343eb6e7fc99be03e4
Aarav Goyal <152468456+gl-aarav@users.noreply.github.com>
2024-09-22 02:58:55 +0000

    Initial commit

---
+++ Q2.java
@@ -0,0 +1,8 @@
+public class Q2{
+  public static void main(String[]args){
+  System.out.println("**********************");
+  System.out.println("*    The Figit Co.   *");
+  System.out.println("* Aarav@thefigit.com *");
+  System.out.println("**********************");
+  }
+}
